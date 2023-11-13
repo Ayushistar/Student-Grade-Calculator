984ee35e3b47a93baeeaadd81357b155c9459aec
Ayushistar <149921128+Ayushistar@users.noreply.github.com>
2023-11-13 12:07:46 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: Take marks obtained (out of 100) in each subject.
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        // Calculate Total Marks: Sum up the marks obtained in all subjects.
+        for (int i = 1; i <= numberOfSubjects; i++) {
+            System.out.print("Enter marks for subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        // Calculate Average Percentage: Divide the total marks by the total number of subjects.
+        double averagePercentage = (double) totalMarks / numberOfSubjects;
+
+        // Grade Calculation: Assign grades based on the average percentage achieved.
+        char grade = calculateGrade(averagePercentage);
+
+        // Display Results: Show the total marks, average percentage, and the corresponding grade to the user.
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    // Method to calculate the grade based on the average percentage.
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
